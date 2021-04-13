912e266c20002c75ac82c4655a61e7c0ef76b0d9
sssua-0928 <hansua990928@gmail.com>
2021-04-14 05:04:37 +0900

    hash 수정

--- Crypto/hash.js
+++ Crypto/hash.js
@@ -2,10 +2,15 @@ const crypto = require('crypto');
 
 const pwd1 = "123123";
 const pwd2 = "12345";
+const pwd3 = "12";
 const base64 = crypto.createHash('sha512').update(pwd1).digest('base64');
 const base64_2 = crypto.createHash('sha512').update(pwd2).digest('base64');
+const base64_3 = crypto.createHash('sha512').update(pwd3).digest('base64');
 const hex = crypto.createHash('sha512').update(pwd1).digest('hex');
+const hex2 = crypto.createHash('sha512').update(pwd2).digest('hex');
 
-console.log(base64);
-console.log(base64_2);
-console.log(hex);
+console.log("pwd1:", base64);
+console.log("pwd3:", base64_2);
+console.log("pwd4:", base64_3);
+console.log("pwd1:", hex);
+console.log("pwd2:", hex2);
